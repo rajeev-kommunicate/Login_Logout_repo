4119fc193ac271389276cf03bd7fe9e14c89c9ce
Rajeev Mishra <rajeev.mishra@kommunicate.io>
2021-10-13 10:03:02 +0530

    First commit
    This is first commit.

---
+++ app/src/main/java/com/example/loginactivity/LoginActivity.java
@@ -0,0 +1,141 @@
+package com.example.loginactivity;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+import com.applozic.mobicomkit.api.account.register.RegistrationResponse;
+
+import io.kommunicate.KmConversationBuilder;
+import io.kommunicate.Kommunicate;
+import io.kommunicate.callbacks.KMLoginHandler;
+import io.kommunicate.callbacks.KmCallback;
+
+
+public class LoginActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    final String TAG = "LoginActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        Button loginButton = findViewById(R.id.buttonLogin);
+        final EditText usernameInput = findViewById(R.id.usernameInput);
+        final EditText passwordInput =  findViewById(R.id.passwordInput);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login(usernameInput.getText().toString(),passwordInput.getText().toString() );
+
+            }
+        });
+
+        TextView registerLink = findViewById(R.id.registrationLink);
+        registerLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegistrationActivity.class));
+            }
+        });
+    }
+
+
+    private void login(String email, String password) {
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            Toast.makeText(LoginActivity.this, "Authentication Success." + user.getEmail(),
+                                    Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(LoginActivity.this, ProfileActivity.class));
+                            finish();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user =  mAuth.getCurrentUser();
+        if(user != null) {
+            startActivity(new Intent(LoginActivity.this, ProfileActivity.class));
+            finish();
+        }
+    }
+
+    public void btnOnClick (View view){
+        Kommunicate.init(this, "f52574e79812dd83de49431ac027ccb0");
+
+        Kommunicate.loginAsVisitor(this, new KMLoginHandler() {
+            @Override
+            public void onSuccess(RegistrationResponse registrationResponse, Context context) {
+                // You can perform operations such as opening the conversation, creating a new conversation or update user details on success
+                new KmConversationBuilder(context)
+                        .setWithPreChat(true)
+                        .launchConversation(new KmCallback() {
+                            @Override
+                            public void onSuccess(Object message) {
+                                Log.d("Conversation", "Success : " + message);
+                            }
+
+                            @Override
+                            public void onFailure(Object error) {
+                                Log.d("Conversation", "Failure : " + error);
+                            }
+                        });
+            }
+
+            @Override
+            public void onFailure(RegistrationResponse registrationResponse, Exception exception) {
+                // You can perform actions such as repeating the login call or throw an error message on failure
+            }
+        });
+
+    }
+
+}
